ea503e1af69f6429c2c0b7d99b516b306b3a7687
esgariot88 <40865390+esgariot88@users.noreply.github.com>
2022-09-28 18:23:10 +0300

    pravka

--- HW1.4.py
+++ HW1.4.py
@@ -3,8 +3,8 @@ quarter = int(input("Введите номер четверти: "))
 if quarter ==1:
     print ("x>0,y>0")
 elif quarter ==2:
-    print ("x>0,y<0")
+    print ("x<0,y>0")
 elif quarter ==3:
     print ("x<0,y<0")
 elif quarter ==4:
-    print ("x<0,y>0")
+    print ("x>0,y<0")
